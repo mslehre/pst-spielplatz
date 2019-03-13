08a9796052f29e87c100948ef9b2898d12fc090b
mario <mario.stanke@uni-greifswald.de>
2019-03-13 12:24:40 +0100

    do.cc program with command-line parsing

--- exercise1/do.cc
+++ exercise1/do.cc
@@ -1,75 +1,76 @@
 #include <iostream>
 #include <getopt.h>
+#include <string>
 
-int main(int argc, char **argv) {
-    int c;
-    int digit_optind = 0;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+bool brief = 0;
+std::string greeting = "";
+double score;
+int digit_optind = 0;
+int c;
     
+void printHelp(){
+    cout << "Options:" << endl
+	 << "\t--brief              Output less information" << endl
+	 << "\t--greeting <string>  A greeting string" << endl
+	 << "\t--score <number>     Set the score" << endl;
+}
+
+void processArgs(int argc, char **argv){
     while (1) {
-        int this_option_optind = optind ? optind : 1;
         int option_index = 0;
         static struct option long_options[] = {
-            {"add",     required_argument, 0,  0 },
-            {"append",  no_argument,       0,  0 },
-            {"delete",  required_argument, 0,  0 },
-            {"verbose", no_argument,       0,  0 },
-            {"create",  required_argument, 0, 'c'},
-            {"file",    required_argument, 0,  0 },
-            {0,         0,                 0,  0 }
+            {"brief",    no_argument,       nullptr, 'y' },
+            {"greeting", optional_argument, nullptr, 'z' },
+            {"score",    optional_argument, nullptr, 's' },
+            {nullptr,          0,           nullptr,  0 }
         };
 
-	c = getopt_long(argc, argv, "abc:d:012",
-			long_options, &option_index);
-        if (c == -1)
-            break;
-
+	c = getopt_long(argc, argv, "", long_options, &option_index);
+	if (c == -1){
+	    break;
+	}
+	
 	switch (c) {
-        case 0:
-            printf("option %s", long_options[option_index].name);
-            if (optarg)
-                printf(" with arg %s", optarg);
-            printf("\n");
+	case 'y':
+	    brief = true;
+	    break;
+	    
+        case 'z':
+	    if (optarg){
+		greeting = optarg;
+	    }
             break;
 
-	case '0':
-        case '1':
-        case '2':
-            if (digit_optind != 0 && digit_optind != this_option_optind)
-		printf("digits occur in two different argv-elements.\n");
-            digit_optind = this_option_optind;
-            printf("option %c\n", c);
-            break;
+	case 's':
+	    if (optarg){
+		score = atof(optarg);
+	    } else {
+		cerr << "Option score specified without value." << endl;
+		printHelp();
+	    }
+	    break;
 
-	case 'a':
-            printf("option a\n");
-            break;
-
-	case 'b':
-            printf("option b\n");
-            break;
-
-	case 'c':
-            printf("option c with value '%s'\n", optarg);
-            break;
-
-	case 'd':
-            printf("option d with value '%s'\n", optarg);
-            break;
-
-	case '?':
-            break;
-
+	case '?': // unrecognized option
 	default:
-            printf("?? getopt returned character code 0%o ??\n", c);
+	    printHelp();
+	    exit(1);
         }
     }
+}
 
-    if (optind < argc) {
-        printf("non-option ARGV-elements: ");
-        while (optind < argc)
-            printf("%s ", argv[optind++]);
-        printf("\n");
+
+int main (int argc, char **argv) {
+    processArgs(argc, argv);
+
+    if (brief){
+	std::cout << "Flag brief is set." << endl;
     }
+    cout << "greeting is: " << greeting << endl;
+    cout << "score is " << score << endl;
 
     return 0;
 }
