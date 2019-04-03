d14ff6b8bb5a55197ff4d98ebb09c9451f9f39b1
MoritzGrillo <mg166257@uni-greifswald.de>
2019-04-03 17:38:31 +0200

    y_offset added

--- exercise1/do.cc
+++ exercise1/do.cc
@@ -11,12 +11,15 @@ std::string greeting = "";
 double score;
 int digit_optind = 0;
 int c;
+int y_offset;
     
 void printHelp(){
     cout << "Options:" << endl
 	 << "\t--brief              Output less information" << endl
 	 << "\t--greeting <string>  A greeting string" << endl
-	 << "\t--score <number>     Set the score" << endl;
+	 << "\t--score <number>     Set the score" << endl
+	 << "\t--y_offset <number>"     << endl;
+
 }
 
 void processArgs(int argc, char **argv){
@@ -26,6 +29,7 @@ void processArgs(int argc, char **argv){
             {"brief",    no_argument,       nullptr, 'y' },
             {"greeting", optional_argument, nullptr, 'z' },
             {"score",    optional_argument, nullptr, 's' },
+			{"y_offset", optional_argument, nullptr, 'a' },
             {nullptr,          0,           nullptr,  0 }
         };
 
@@ -53,6 +57,15 @@ void processArgs(int argc, char **argv){
 		printHelp();
 	    }
 	    break;
+	
+	case 'a':
+		if (optarg){
+		y_offset = atof(optarg);
+	    } else {
+		cerr << "Y_offset specified without value" << endl;
+		printHelp();
+	    }
+	    break;
 
 	case '?': // unrecognized option
 	default:
@@ -71,6 +84,7 @@ int main (int argc, char **argv) {
     }
     cout << "greeting is: " << greeting << endl;
     cout << "score is " << score << endl;
+	cout << "y_offset is " << y_offset << endl;
 
     return 0;
 }
