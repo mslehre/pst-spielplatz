cb9a70a52029e8cb3640312f6ff4299e435d91dd
Petula <petula.diemke@stud.uni-greifswald.de>
2021-04-09 17:34:42 +0200

    add option numthreads

--- exercise1/do.cc
+++ exercise1/do.cc
@@ -11,79 +11,92 @@ std::string greeting = "";
 double score;
 int digit_optind = 0;
 int c;
+unsigned int numthreads;
 double threshold;
-    
+
 void printHelp(){
-    cout << "Options:" << endl
-	 << "\t--brief              Output less information" << endl
-	 << "\t--greeting <string>  A greeting string" << endl
-	 << "\t--score <number>     Set the score" << endl
-	 << "\t--threshold <number>     Set the threshold" << endl;
+	cout << "Options:" << endl
+	     << "\t--brief                 Output less information" << endl
+	     << "\t--greeting <string>     A greeting string" << endl
+	     << "\t--score <number>        Set the score" << endl
+	     << "\t--numthreads <integer>  Set the number of threads" << endl
+	     << "\t--threshold <number>    Set the threshold" << endl;
 }
 
 void processArgs(int argc, char **argv){
-    while (1) {
-        int option_index = 0;
-        static struct option long_options[] = {
-            {"brief",    no_argument,       nullptr, 'y' },
-            {"greeting", optional_argument, nullptr, 'z' },
-            {"score",    optional_argument, nullptr, 's' },
-            {"threshold",    optional_argument, nullptr, 'c' },
-            {nullptr,          0,           nullptr,  0 }
-        };
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"brief",      no_argument,       nullptr, 'y' },
+			{"greeting",   optional_argument, nullptr, 'z' },
+			{"score",      optional_argument, nullptr, 's' },
+			{"numthreads", optional_argument, nullptr, 'a' },
+			{"threshold",  optional_argument, nullptr, 'c' },
+			{nullptr,             0,          nullptr,  0  }
+		};
 
-	c = getopt_long(argc, argv, "", long_options, &option_index);
-	if (c == -1){
-	    break;
+		c = getopt_long(argc, argv, "", long_options, &option_index);
+		if (c == -1){
+			break;
+		}
+
+		switch (c) {
+		case 'y':
+			brief = true;
+			break;
+
+		case 'z':
+			if (optarg){
+				greeting = optarg;
+			}
+			break;
+
+		case 's':
+			if (optarg){
+				score = atof(optarg);
+			} else {
+				cerr << "Option score specified without value." << endl;
+				printHelp();
+			}
+			break;
+
+		case 'a':
+			if (optarg){
+				numthreads = atoi(optarg);
+			} else {
+				cerr << "Option numthreads specified without value." << endl;
+				printHelp();
+			}
+			break;
+
+		case 'c':
+			if (optarg){
+				threshold = atof(optarg);
+			} else {
+				cerr << "Option threshold specified without value." << endl;
+				printHelp();
+			}
+			break;
+
+		case '?': // unrecognized option
+		default:
+			printHelp();
+			exit(1);
+		}
 	}
-	
-	switch (c) {
-	case 'y':
-	    brief = true;
-	    break;
-	    
-        case 'z':
-	    if (optarg){
-		greeting = optarg;
-	    }
-            break;
-
-	case 's':
-	    if (optarg){
-		score = atof(optarg);
-	    } else {
-		cerr << "Option score specified without value." << endl;
-		printHelp();
-	    }
-	    break;
-
-	case 'c':
-	    if (optarg){
-		threshold = atof(optarg);
-	    } else {
-		cerr << "Option threshold specified without value." << endl;
-		printHelp();
-	    }
-	    break;
-
-	case '?': // unrecognized option
-	default:
-	    printHelp();
-	    exit(1);
-        }
-    }
 }
 
 
 int main (int argc, char **argv) {
-    processArgs(argc, argv);
+	processArgs(argc, argv);
 
-    if (brief){
+	if (brief){
 	std::cout << "Flag brief is set." << endl;
-    }
-    cout << "greeting is: " << greeting << endl;
-    cout << "score is " << score << endl;
-    cout << "threshold is " << threshold << endl;
+	}
+	cout << "greeting is: " << greeting << endl;
+	cout << "score is " << score << endl;
+	cout << "numthreads is " << numthreads << endl;
+	cout << "threshold is " << threshold << endl;
 
-    return 0;
+	return 0;
 }
